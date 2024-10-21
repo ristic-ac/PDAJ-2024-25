979fe2b9b0b281db7645caabbc1beb356993d2bb
ristic-ac <ristic.in30.2019@uns.ac.rs>
2024-10-21 19:57:52 +0200

    vezbe 3 initial

---
+++ 03/vezbe_03/src/bin/01.rs
@@ -0,0 +1,17 @@
+fn main(){
+    // Define the slice s, which refers the the string literal
+    let s = "hello";
+    // Valid until the end of the scope
+    println!("{}",s);
+    // Dropping the slice because it goes out of scope (isnt used anymore)
+
+    // Define string s, which is mutable
+    let mut s = String::from("hello");
+
+    // push_str() appends a literal to a String
+    s.push_str(", world!"); 
+
+    // This will print `hello, world!`
+    println!("{s}"); 
+
+}
---
+++ 03/vezbe_03/src/bin/02.rs
@@ -0,0 +1,14 @@
+fn main(){
+    // Integers implement the Copy trait, so the value is copied
+    let x = 5;
+    let y = x;
+    println!("{}", x);
+    println!("{}", y);
+
+    // Strings do not implement the Copy trait, so the value is moved
+    let s1 = String::from("hello");
+    // let s2 = s1;
+    let s2 = s1.clone();
+    println!("{s1}, world!");
+    println!("{s2}, world!");
+}
