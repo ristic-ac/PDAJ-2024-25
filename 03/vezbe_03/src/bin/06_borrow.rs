4c9bcd3c59d7d380c2860863058f37330ebec0e4
ristic-branislav <branislav.ristic@42computing.tech>
2024-10-22 15:16:44 +0200

    update

--- 03/vezbe_03/src/bin/06_borrow.rs
+++ 03/vezbe_03/src/bin/06_borrow.rs
@@ -4,6 +4,13 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
+
+    let i1 = 5;
+    let i2 = &i1;
+    let i3 = *i2; // Value is copied
+    println!("The value of i1 is {i1}.");
+    println!("The value of i2 is {i2}.");
+    println!("The value of i3 is {i3}.");
 }
 
 fn calculate_length(s: &String) -> usize {
