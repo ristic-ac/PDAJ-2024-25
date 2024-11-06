1a980b9c5a53149d5434ef0e15e1f07aeccd1862
ristic-branislav <branislav.ristic@42computing.tech>
2024-11-06 17:26:28 +0100

    fix

--- 05/vezbe_05/src/bin/18_a_unwrap.rs
+++ 05/vezbe_05/src/bin/18_a_unwrap.rs
@@ -1,15 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
+// The unwrap method is a shortcut method implemented just like the match expression. If the Result value is the Ok variant, unwrap will return the value inside the Ok. If the Result is the Err variant, unwrap will call the panic! macro for us.
 fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {error:?}");
-            })
-        } else {
-            panic!("Problem opening the file: {error:?}");
-        }
-    });
-    println!("{:?}", greeting_file);
+    let greeting_file = File::open("hello.txt").unwrap();
 }
